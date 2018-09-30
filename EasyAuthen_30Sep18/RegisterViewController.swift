920e80fc32bc02caad3a58f14ceed8686f0f74f5
masterUNG <phrombutr@gmail.com>
2018-09-30 10:09:17 +0700

    Step1 OK

---
+++ EasyAuthen_30Sep18/RegisterViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterViewController.swift
+//  EasyAuthen_30Sep18
+//
+//  Created by MasterUNG on 30/9/2561 BE.
+//  Copyright © 2561 MasterUNG. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
