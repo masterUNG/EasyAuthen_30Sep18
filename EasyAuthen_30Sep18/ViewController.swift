99400d00000b5340008552b799842bc1ec3d2eb1
masterUNG <phrombutr@gmail.com>
2018-10-28 14:51:47 +0700

    Success 28oct

--- EasyAuthen_30Sep18/ViewController.swift
+++ EasyAuthen_30Sep18/ViewController.swift
@@ -13,10 +13,7 @@ class ViewController: UIViewController {
 //    Explicit
     var userString: String = ""
     var passwordString: String = ""
-    var prefixString: String = "https://www.androidthai.in.th/pen/getAllData.php"
-    
-    
-
+    var prefixString: String = "https://www.androidthai.in.th/pen/getDataWhere1Column.php?isAdd=true&User="
     
     @IBOutlet weak var usetTextFiled: UITextField!
     
@@ -32,7 +29,7 @@ class ViewController: UIViewController {
         userString = usetTextFiled.text!
         passwordString = passwordTextField.text!
         
-        let postURLString: String = prefixString
+        let postURLString: String = prefixString + userString
         print("postURLString ==> \(postURLString)")
         
         let urlPHP = URL(string: postURLString)
@@ -50,37 +47,60 @@ class ViewController: UIViewController {
                     let jsonString: String = canReadData! as String
                     print("jsonString ==> \(jsonString)")
                     
+                    let blockPrefix = "["
+                    var noPrefixJson: String = ""
+                    
+                    if let myContentArray = canReadData?.components(separatedBy: blockPrefix) {
+                        noPrefixJson = myContentArray[1]
+                        print("noPrefixJson ==> \(noPrefixJson)")
+                    }
+                    
+                    let blockSubfix = "]"
+                    var noSubfixJson = ""
+//                    let noPrefixJson1 = noPrefixJson as String
+                    
+                    
+                    let myContentArray2 = noPrefixJson.components(separatedBy: blockSubfix)
+                    noSubfixJson = myContentArray2[0]
+                    print("noSubfixJson ==> \(noSubfixJson)")
+                    
                     let myComma = ","
-                    if let contentArray = canReadData?.components(separatedBy: myComma) {
+                    let contentArray = noSubfixJson.components(separatedBy: myComma)
+                        
+                        var indexInt = 0
                         
                         for test in contentArray {
                             print("test ==> \(test)")
-                        }
-                        let showName = "{" + contentArray[1] + "}"
+                            
+                            print("contentArray[\(indexInt)] ==> \(contentArray[indexInt])")
+                            
+                            indexInt += 1
+                        }   // for
                         
-                        self.convertJSONtoDictionary(jsonText: showName)
+//                        let showName = "{" + contentArray[0] + "}"
+//                        print("showName ==> \(showName)")
+                        self.convertJSONtoDictionary(jsonText: noSubfixJson)
                         
-                    } // if
+                    
                 }   //if
             }   // if else
-            
-            
         }   // task
         task.resume()
-        
-        
-        
-        
         
     }   // loginButton
     
     func convertJSONtoDictionary(jsonText: String) -> Void {
 //        let jsonText = "{\"first_name\":\"Sergey\"}"
-        print("jsonText ==> \(jsonText)")
+       print("jsonText Master ==> \(jsonText)")
+        
+//        let testJSON = "{\"first_name\":\"Sergey\",\"first_name2\":\"Sergey2\",\"first_name3\":\"Sergey3\"}"
+//        print("testJSON ==> \(testJSON)")
+        
         var dictonary:NSDictionary?
         
-        if let data = jsonText.data(using: String.Encoding.utf8) {
-            
+//        if let data = jsonText.data(using: String.Encoding.utf8) {
+         if let data = jsonText.data(using: String.Encoding.utf8) {
+        
             do {
                 dictonary = try JSONSerialization.jsonObject(with: data, options: []) as? [String:AnyObject] as NSDictionary?
                 
